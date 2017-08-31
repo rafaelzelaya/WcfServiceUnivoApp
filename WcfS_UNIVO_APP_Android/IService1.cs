cfece987a36713fd99ec0dfe5b601ae941c630af
rafaelzelaya <rafaelzelayax@gmail.com>
2017-08-31 09:41:11 -0600

    Add project files.

---
+++ WcfS_UNIVO_APP_Android/IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfS_UNIVO_APP_Android
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign)]
+    public interface IService1
+    {
+        [OperationContract(ProtectionLevel =System.Net.Security.ProtectionLevel.Sign)]
+        void SegruridadPorControlGranular();
+
+        [OperationContract]
+        String ComprobarEstudiante(String usuario, String clave);
+
+        [OperationContract]
+        Boolean MateriasInscritas(String carnet, String clave);
+
+        [OperationContract]
+        void Notas(String carnet, String clave);
+
+        [OperationContract]
+        void PagosRealizados(String carnet, String clave);
+
+        [OperationContract]
+        void PerfilEstudiante(String carnet, String clave);
+        
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfS_UNIVO_APP_Android/Service1.svc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfS_UNIVO_APP_Android
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class 
+    // name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, 
+    // please select Service1.svc or Service1.svc.cs at the Solution Explorer and start 
+    // debugging.
+    public class Service1 : IService1
+    {
+     
+        /// <summary>
+        /// Loguear al estudiante
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <param name="clave"></param>
+        /// <returns></returns>
+        public String ComprobarEstudiante(String usuario, String clave)
+        {
+            if(usuario == "U20120453" && clave == "12345")
+            {
+                return OperationContext.Current.SessionId;
+            }
+            return "NoId";
+        }
+
+        /// <summary>
+        /// Debe retornar aulas materias y horas
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="clave">Con el objeto de comprobar cada que necesite datos
+        /// de la cuenta</param>
+        /// <returns></returns>
+        public Boolean MateriasInscritas(String carnet, String clave){
+            return true;
+        }
+
+        /// <summary>
+        /// Obtendra las notas de laboratorios, parciales, reposiciones, nota final
+        /// </summary>
+        /// <param name="carnet"></param>
+        /// <param name="clave"></param>
+        public void Notas(String carnet, String clave) {
+
+        }
+
+        /// <summary>
+        /// Los pagos realizados del ciclo activo supongo.
+        /// </summary>
+        /// <param name="carnet"></param>
+        /// <param name="clave"></param>
+        public void PagosRealizados(String carnet, String clave) { }
+
+        /// <summary>
+        /// Los datos del perfil de estudiante, nose si seria apropiado que envie tambien
+        /// la imagen del estudiante.
+        /// </summary>
+        /// <param name="carnet"></param>
+        /// <param name="clave"></param>
+        /// <returns></returns>
+        public void PerfilEstudiante(String carnet, String clave) { }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
